e8ff5051a24b89413dd733d5f66776c89164f16a
demis27@gmail.com <demis27@gmail.com@d9739d13-6778-f0a2-71db-929e711a67ee>
2014-11-11 16:32:41 +0000

    continue client

---
+++ familh/client/src/main/resources/javascript/familh.js
@@ -0,0 +1,15 @@
+var modal = false;
+
+function openCreateUser() {
+    console.log('openCreateUser()');
+    $("#page").addClass('blur');
+    modal = true;
+}
+
+function onPage() {
+}
+
+function unblur() {
+}
+
+
