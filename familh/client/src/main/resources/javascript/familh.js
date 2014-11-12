c201bd0122ee163ec1f0bfea22043a7782dc38dc
demis27@gmail.com <demis27@gmail.com@d9739d13-6778-f0a2-71db-929e711a67ee>
2014-11-12 15:06:38 +0000

    continue clien

--- familh/client/src/main/resources/javascript/familh.js
+++ familh/client/src/main/resources/javascript/familh.js
@@ -6,6 +6,11 @@ function openCreateUser() {
     $("#create-user").show();
 }
 
+function openLoginUser() {
+    modal = true;
+    $("#log-in-user").show();
+}
+
 function onPage() {
     if (modal) {
         blurPage();
@@ -24,3 +29,28 @@ function blurPage() {
     $("#page").addClass('blur');
     modal = false;
 }
+
+function connectUser() {
+
+    if (authenticationValid($("#log-in-user-login").val(), $("#log-in-user-password").val())) {
+        onPage();
+        $("#header-user-profile").show();
+        $("#header-user-logout").show();
+        $("#header-create-user").hide()
+        $("#header-user-sign-in").hide();
+    }
+
+
+}
+
+function authenticationValid(login, password) {
+    return true;
+}
+
+function logoutUser() {
+        $("#header-user-profile").hide();
+        $("#header-user-logout").hide();
+        $("#header-create-user").show()
+        $("#header-user-sign-in").show();
+}
+
